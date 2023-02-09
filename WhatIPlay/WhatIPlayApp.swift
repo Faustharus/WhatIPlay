a534989d152eb83bae25ac1c99232adf5f8ab99d
Dams920 <50359853+Dams920@users.noreply.github.com>
2023-02-09 15:18:56 +0100

    Initial Commit

---
+++ WhatIPlay/WhatIPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIPlayApp.swift
+//  WhatIPlay
+//
+//  Created by Damien Chailloleau on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatIPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
